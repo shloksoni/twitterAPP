f91cfbc85437b7d21c1ac20ab1b3cec86942f46c
shubham <khakharshubham@gmail.com>
2020-06-06 21:48:46 +0530

    trying to merge

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const knex=require('knex');
+const db=knex(
+  {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '3012',
+      database : 'postgres'
+  }
+});
+let tweet=``;
 
+console.log(tweet);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.select('tweet').from('twitter.tweets').then(data=>res.send(data));
+  //res.render('index', { title: 'Express' });
+  
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const knex=require('knex');
+const db=knex(
+  {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '3012',
+      database : 'postgres'
+  }
+});
+db.select('tweet').from('twitter.tweets').where(`username=${}`)
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
